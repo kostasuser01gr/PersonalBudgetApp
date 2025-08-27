15bb7d8976c8e3d49e8eb76e87caa6b36e4a98bf
K1 <75499796+kostasuser01gr@users.noreply.github.com>
2025-08-27 09:29:39 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Μπορείς να προσθέσεις εδώ ό,τι άλλο configuration χρειάζεσαι
 };
 
 export default nextConfig;
