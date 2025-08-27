a5ad889419500aa55c4decb250541174fc25ae2d
K1 <75499796+kostasuser01gr@users.noreply.github.com>
2025-08-27 12:23:07 +0300

    Update and rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// .eslintrc.js
+module.exports = {
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+  }
+}
